e218735c0784e4cf0afbffba2a5aad72d501a845
Anaid Candido <anaid.candido@gmail.com>
2022-07-25 12:28:36 +0100

    run prettier

--- src/components/AppHeader.tsx
+++ src/components/AppHeader.tsx
@@ -19,11 +19,11 @@ interface AppHeaderProps {
   }
 }
 */
- export default function AppHeader({ title }: AppHeaderProps): JSX.Element{
+export default function AppHeader({ title }: AppHeaderProps): JSX.Element {
   return (
     <header>
       <h1>{title}</h1>
       {title.length < 5 && <p>(The title is a work-in-progress)</p>}
     </header>
-  )
- }
+  );
+}

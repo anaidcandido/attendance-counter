f1f4d4fa90c8d90c513d6afccd510d7cd0d35d5b
Anaid Candido <anaid.candido@gmail.com>
2022-07-25 12:35:03 +0100

    replaced if/else with &&

--- src/components/CopyrightNotice.tsx
+++ src/components/CopyrightNotice.tsx
@@ -8,20 +8,28 @@ export default function CopyrightNotice({
   author,
   year,
 }: CopyrightNoticeProps): JSX.Element {
+  return (
+    <>
+    <p>
+    Copyright {year}, {author}
+    {author = "" && <p>Copyright {year} (but to nobody in particular)</p>}
+    </p>
+    </>
+  )
   // truthiness check: https://developer.mozilla.org/en-US/docs/Glossary/Truthy
-  if (author) {
-    return (
-      <>
-        <p>
-          Copyright {year}, {author}
-        </p>
-      </>
-    );
-  } else {
-    return (
-      <>
-        <p>Copyright {year} (but to nobody in particular)</p>
-      </>
-    );
-  }
+  //if (author) {
+  //  return (
+  //    <>
+  //      <p>
+  //        Copyright {year}, {author}
+  //      </p>
+  //    </>
+  //  );
+  //} else {
+  //  return (
+  //    <>
+  //      <p>Copyright {year} (but to nobody in particular)</p>
+  //    </>
+  //  );
+  //}
 }
